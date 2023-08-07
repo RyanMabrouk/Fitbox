ea60c4d3fe0ad92037f2859a1b735f1b2c8bd628
RyanMabrouk <mabroukr999@gmail.com>
2023-08-08 00:18:11 +0100

    Update SignUp.jsx

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer/Footer";
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { user, signUp } = UserAuth();
+  const { signUp } = UserAuth();
   const navigate = useNavigate();
 
   const goTop = () => {
