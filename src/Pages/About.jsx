6b4082fbb87ef02d77121f5dfc3f2473811d510a
RyanMabrouk <mabroukr999@gmail.com>
2023-08-08 01:19:21 +0100

    fix

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -43,7 +43,7 @@ function About() {
               </p>
               <MainButton
                 color={`!text-white`}
-                bg={`bg-[#ff0336]`}
+                bg={`bg-[#eab308]`}
                 goTo="/contact"
                 text="contact us"
                 arrowColor={`!text-white`}
